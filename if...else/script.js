6325f355801cd9e0c3e49c06b4816b9592f80c30
heymanta1 <hemantasubedi2062@gmail.com>
2026-07-24 20:21:13 +0545

    if..else in javascript

---
+++ if...else/script.js
@@ -0,0 +1,7 @@
+let num=Number(prompt("Enter a number"));
+if(num>0){
+    document.write("The number " + num + " is an positive number");
+}
+else{
+    document.write("The number " + num +" is negative number" );
+}
