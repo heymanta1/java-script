57e71cbf15385827ed0278c07bcf6ef9bee1fb31
heymanta1 <hemantasubedi2062@gmail.com>
2026-07-24 20:06:00 +0545

    Arithemitic operators

---
+++ Arithmetic Operators/script.js
@@ -0,0 +1,8 @@
+let a=Number(prompt("Enter the value of a"));
+let b=Number(prompt("Enter the value of b"));
+document.write("The Addition"+ a +" and"+ b +"is=",(a+b)+"<br>");
+document.write("The Subtraction "+ a +" and"+ b +"is=",(a-b)+"<br>");
+document.write("The Multiplication"+ a +" and"+ b +"is=",(a*b)+"<br>");
+document.write("The Division"+ a +" and " + b +"is=",(a/b)+"<br>");
+document.write("The Modulus"+ a +" and " + b +"is=",(a%b)+"<br>");
+document.write("The Power"+ a +" and" + b +"is=",(a**b)+"<br>");
