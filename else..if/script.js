7fe92e2b6ead41895a9644e12a5b4df714cd1627
heymanta1 <hemantasubedi2062@gmail.com>
2026-07-25 19:00:34 +0545

    Else..if in javascript

---
+++ else..if/script.js
@@ -0,0 +1,16 @@
+let marks =Number(prompt("Enter the marks "));
+if(marks>80){
+    document.write("Your Grade in A ");
+}
+else if(marks>=75){
+    document.write("You Grade is B+ ")
+}
+else if(marks>=60){
+    document.write("Your Grade is B")
+}
+else if(marks>=40){
+    document.write("Your Grade is C");
+}
+else{
+    document.write("Fail");
+}
