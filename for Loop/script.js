bf24013eee2fb2dd3b88a44bd4170986f826784f
heymanta1 <hemantasubedi2062@gmail.com>
2026-07-26 21:54:51 +0545

    for loop in java script

---
+++ for Loop/script.js
@@ -0,0 +1,5 @@
+let a=String(prompt("Enter the name for the for loop"));
+let num=Number(prompt("Enter the no.of times you want to repeat"));
+for(i=1;i<=num;i++){
+    document.write( i +". " + a + "<br>");
+}
