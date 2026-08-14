5f4e0a332f7bb803c1b85188d96ad9aaac319c36
heymanta1 <hemantasubedi2062@gmail.com>
2026-08-14 13:56:44 +0545

    Basic Array in javaScript

---
+++ Array/basicArray.js
@@ -0,0 +1,11 @@
+// What is array?
+// An array is used to store multiple values in one variable.
+// for eg:
+// // Without an array:
+//     let student1 = "Ram";
+//     let student2 = "Shyam";
+//     let student3 = "Hari";
+// with array:
+let students = ["Ram", "Shyam", "Hari"];
+
+console.log(students);
