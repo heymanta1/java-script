aa5ebc008dbde31e93fdc5b333d57fcb1f8db5f9
heymanta1 <hemantasubedi2062@gmail.com>
2026-08-14 14:16:18 +0545

    solved question related to the array

---
+++ Array/Arrayquestion.js
@@ -0,0 +1,14 @@
+// Write a program that:
+
+// Creates an array called fruits
+// Adds "Mango" using push()
+// Removes the last fruit using pop()
+// Prints the final array
+// Prints the number of fruits using .length
+
+let fruits =["Apple","Orange","Banana"];
+fruits.push("Mango");
+console.log(fruits)
+fruits.pop();
+console.log(fruits)
+console.log(fruits.length)
