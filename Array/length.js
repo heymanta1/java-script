5d04c78cdff8362ea74c3179fb14ff5a535c85d3
heymanta1 <hemantasubedi2062@gmail.com>
2026-08-14 14:04:42 +0545

    .length function in Array

--- Array/length.js
+++ Array/length.js
@@ -1,3 +1,7 @@
 // .length tells us how many items are inside an array.
 let students = ["Ram", "Shyam", "Hari"];
 console.log(students.length);
+
+let numbers = [10, 20, 30, 40, 50];
+
+console.log(numbers.length);
