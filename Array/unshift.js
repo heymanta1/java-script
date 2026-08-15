eb0bc641c4edd3a845e672c32f50946ca50a9f9d
heymanta1 <hemantasubedi2062@gmail.com>
2026-08-15 12:24:45 +0545

    unshift() function using js

---
+++ Array/unshift.js
@@ -0,0 +1,11 @@
+// // unshift() — Add to the beginning
+
+// unshift() adds a new item at the start of an array.
+
+let fruits = ["Banana", "Mango"];
+
+console.log(fruits);
+
+fruits.unshift("Apple");
+
+console.log(fruits);
