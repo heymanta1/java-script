9ba633f46dded0762d41c79f30f2f46795639780
heymanta1 <hemantasubedi2062@gmail.com>
2026-08-15 12:34:26 +0545

    slice question practice

--- Array/slice.js
+++ Array/slice.js
@@ -7,3 +7,20 @@ let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
 let result = fruits.slice(1, 4);
 
 console.log(result);
+
+
+// // slice() Practice Questions
+// 1. What is the output?
+let fruitss = ["Apple", "Banana", "Mango", "Orange"];
+console.log(fruitss.slice(1, 3));
+
+// output ["Banana","Mango"]
+
+let numbers = [10, 20, 30, 40, 50];
+console.log(numbers.slice(0, 3));
+
+//Answer [10,20,30]
+
+let colors = ["Red", "Blue", "Green", "Yellow", "Black"];
+console.log(colors.slice(1));
+// Answer [ 'Blue', 'Green', 'Yellow', 'Black' ]
