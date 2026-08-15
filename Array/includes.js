8ae52161373748fe0564d84573e645ee1e276635
heymanta1 <hemantasubedi2062@gmail.com>
2026-08-15 12:57:09 +0545

    includes() function using java script

---
+++ Array/includes.js
@@ -0,0 +1,16 @@
+// includes() — Check if an item exists
+
+// includes() checks whether an array contains a particular value.
+
+// It returns either:
+
+// true → item exists
+// false → item doesn't exist
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log(fruits.includes("Mango"));
+
+let fruitss = ["Apple", "Banana", "Mango"];
+
+console.log(fruitss.includes("Orange"));
