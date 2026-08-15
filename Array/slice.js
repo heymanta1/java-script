251383162eb128ad2373cb451b67e467a61cfcae
heymanta1 <hemantasubedi2062@gmail.com>
2026-08-15 12:26:44 +0545

    slice function using javascript

---
+++ Array/slice.js
@@ -0,0 +1,9 @@
+// slice() — Get a part of an array
+
+// slice() lets you copy a portion of an array without changing the original array.
+
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+let result = fruits.slice(1, 4);
+
+console.log(result);
