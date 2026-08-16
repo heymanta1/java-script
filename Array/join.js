fee245b9ce6fcdf29f6b60b981fe479feaf824b6
heymanta1 <hemantasubedi2062@gmail.com>
2026-08-16 18:57:47 +0545

    practice join () function in javascript

--- Array/join.js
+++ Array/join.js
@@ -3,3 +3,15 @@
 let fruits = ["Apple", "Mango", "Banana"];
 
 console.log(fruits.join());
+
+
+
+// practice question
+
+let students = ["Ram", "Shyam", "Hari", "Sita"];
+
+// Q2. Join all students using " | ".
+// Expected:
+// Ram | Shyam | Hari | Sita
+
+console.log(students.join());
