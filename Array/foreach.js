e7dab7986cb6e8925b44e11f81ab74222269e6eb
heymanta1 <hemantasubedi2062@gmail.com>
2026-08-16 18:59:30 +0545

    Foreach () function in javascript

---
+++ Array/foreach.js
@@ -0,0 +1,7 @@
+ // forEach() is used to run some code for every item in an array.
+
+ let students = ["Ram", "Shyam", "Hari", "Sita"];
+
+students.forEach(function(student) {
+    console.log(student);
+});
