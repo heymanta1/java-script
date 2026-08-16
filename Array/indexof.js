55228bdaad3be3cb7e26a5618ba84fe366b673d3
heymanta1 <hemantasubedi2062@gmail.com>
2026-08-16 18:15:39 +0545

    practice question solved of indexof() function in javascript

--- Array/indexof.js
+++ Array/indexof.js
@@ -2,3 +2,11 @@
 let students = ["Ram", "Shyam", "Hari", "Sita"];
 
 console.log(students.indexOf("Hari"));
+
+//practice question
+let studentss = ["Ram", "Shyam", "Hari", "Sita"];
+
+// Q1. Find the index of "Sita" using indexOf().
+// Expected:
+// 3
+console.log(studentss.indexOf("Sita"));
