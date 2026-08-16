0d3527eb555128e5f068cbf9922e3e4cf8e91a74
heymanta1 <hemantasubedi2062@gmail.com>
2026-08-16 18:22:47 +0545

    practice question solved of concat() function in javascript

--- Array/concat.js
+++ Array/concat.js
@@ -5,3 +5,14 @@ let girls = ["Sita", "Gita"];
 let students = boys.concat(girls);
 
 console.log(students);
+
+// practice question 
+
+let fruits = ["Apple", "Mango"];
+let vegetables = ["Potato", "Carrot"];
+
+// Q1. Combine both arrays using concat().
+// Expected:
+// ["Apple", "Mango", "Potato", "Carrot"]
+let foods =fruits.concat(vegetables);
+console.log(foods);
